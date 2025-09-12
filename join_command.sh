fbcebdb038875cd5d611b841f4d49254c4d245aa
AdidelaHarishReddy <adidelaharish201@gmail.com>
2025-09-12 22:50:47 +0530

    update

--- join_command.sh
+++ join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.0.93:6443 --token daz1do.u4kfuef9mg0sotdm --discovery-token-ca-cert-hash sha256:14cc800775d8367716be02af27fb0f79f79da48b36abebd81c1f3a7dc75bca44 
+kubeadm join 10.0.0.16:6443 --token 0p33zl.7z3k3iwwtpjnve1k --discovery-token-ca-cert-hash sha256:2871dc7c729fe326309872bc98f9de366acf74f7d04b85e3c748134eb185f6eb 
